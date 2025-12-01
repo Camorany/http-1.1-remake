47b3bb044cb91406df03faf8735a62f8bea41674
Cameron Lane <s5404862@bournemouth.ac.uk>
2025-12-01 20:47:28 +0000

    added basic udp text reader class

--- cmd/udplistener/main.go
+++ cmd/udplistener/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello from the UDP Listener!")
+	udpAddr, addrErr := net.ResolveUDPAddr("udp", "localhost:42069")
+
+	if addrErr != nil {
+		panic(udpAddr)
+	}
+
+	udpConn, connErr := net.DialUDP("udp", nil, udpAddr)
+
+	if connErr != nil {
+		panic(udpAddr)
+	}
+
+	defer udpConn.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Println(">")
+		content, readErr := reader.ReadString('\n')
+
+		if readErr != nil {
+			panic(readErr)
+		}
+
+		udpConn.Write([]byte(content))
+	}
+
 }
