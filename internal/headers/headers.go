d937a7a7de93d2bd27b8aa3ac60e6ad10f96bd75
Camorany <Cjlane9090@gmail.com>
2026-02-21 16:33:48 +0000

    Added header parser logic

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,5 +1,10 @@
 package headers
 
+import (
+	"errors"
+	"strings"
+)
+
 type Headers map[string]string
 
 func NewHeaders() Headers {
@@ -7,6 +12,49 @@ func NewHeaders() Headers {
 }
 
 func (h Headers) Parse(data []byte) (n int, done bool, err error) {
+	n = 0
+	done = false
+	err = nil
 
-	return
+	// If data is missing CRLF, not enough data to parse yet
+	endIndex := strings.Index(string(data), "\r\n")
+	if endIndex == -1 {
+		return n, done, err
+	}
+
+	// Get individual headers as strings
+	headerStrings := strings.Split(string(data), "\r\n")
+
+	// Parse each headerString into header map
+	for _, headerString := range headerStrings {
+
+		//
+		if headerString == "" {
+			break
+		}
+
+		// Getting field-line and (trimmed) field-value
+		splitStrings := strings.SplitN(headerString, ":", 2)
+		headerFieldLine := splitStrings[0]
+		headerFieldValue := strings.TrimSpace(splitStrings[1])
+
+		// If header hasn't been split into a field-line and field-value only, throw error
+		if len(splitStrings) != 2 {
+			err = errors.New("string splitting error")
+			return n, done, err
+		}
+
+		// If field-line contains whitespace
+		if strings.Contains(headerFieldLine, " ") {
+			err = errors.New("field-line formatting error")
+			return n, done, err
+		}
+
+		// Add field-line and field-value to headers map
+		h[headerFieldLine] = headerFieldValue
+
+	}
+
+	n = len(headerStrings[0]) + 2
+	return n, done, err
 }
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func testRequestHeader(t *testing.T) {
+func TestRequestHeader(t *testing.T) {
 	// Test: Valid single header
 	headers := NewHeaders()
 	data := []byte("Host: localhost:42069\r\n\r\n")
